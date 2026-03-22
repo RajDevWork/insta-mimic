989546fd07c60177207c12d2079125dbd3a210e6
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-22 20:28:33 +0530

    Created a user model to store user data

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,'Username is required'],
+        unique:[true,'Username is already exists!']
+    },
+    email:{
+        type:String,
+        required:[true,'Email is required'],
+        unique:[true,"Email already exists!"]
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required']
+    },
+    bio:{
+        type:String
+    },
+    prifile_image:{
+        type:String,
+        default:"https://ik.imagekit.io/ksr4wjoxx/insta-clone-mern/profile-image.webp?updatedAt=1771491195607"
+    }
+},{
+    timestamps:true
+})
+
+const userModel = mongoose.model('users',userSchema)
+
+module.exports = userModel
