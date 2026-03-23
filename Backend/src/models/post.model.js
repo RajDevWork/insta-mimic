f7133648fab91e1e793c3b219aea54e4f19220e6
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-23 11:44:38 +0530

    Post model created

---
+++ Backend/src/models/post.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    caption:{
+        type:String,
+        default:""
+    },
+    imgUrl:{
+        type:String,
+        required:[true,'imgUrl is missing to create a post']
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:[true,'User id is missing to create a post']
+    }
+},{
+    timestamps:true
+})
+
+const postModel = mongoose.model("posts",postSchema)
+
+module.exports = postModel
