80c8b19526c1c34e805e1f4c1d03316ba39f7b3b
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-30 20:17:42 +0530

    added 4 layer archi and implemented on login page

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/form.scss'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 
 const Login = () => {
-  const handleSubmit =(e)=>{
+
+    const {user,loading,handleLogin} = useAuth()
+    const [usename, setUsename] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+
+  const handleSubmit = async(e)=>{
     e.preventDefault();
+    await handleLogin(usename,password)
+    console.log("User loggedin")
+    navigate("/")
   }
 
   return (
     <main>
         <div className="form-container">
+            {
+                loading ? <h1>Loading...</h1>:''
+            }
+
+
             <h1>Login</h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="username" placeholder='Enter username' />
-                <input type="password" name="password" placeholder='Enter password' />
+                <input type="text"
+                 name="username"
+                 placeholder='Enter username'
+                 value={usename}
+                 onChange={(e)=>setUsename(e.target.value)}
+                 />
+                <input type="password" 
+                name="password" 
+                placeholder='Enter password'
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                />
                 <button className='button button-primary'>Login</button>
             </form>
             <p>Don't have an account ? <Link to={"/register"}>Create One</Link></p>
